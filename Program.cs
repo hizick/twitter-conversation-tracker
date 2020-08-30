1e42b5581cb98f8c50dc643ca8a79be9d9d1eacd
isaac ikusika <isaacikusika@gmail.com>
2020-08-30 16:38:14 +0100

    changes on nuget

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ namespace OriginalTweetFinder
 {
     class Program
     {
-        private static string CONSUMER_KEY = "CjIO1ihBhRmgFaS7FuOHioMbq";
-        private static string CONSUMER_SECRET = "8nrYdBY0jkCUYnPklrjmkdtGvBFb1Y8pP4Dc8JkluOB8RWPRKu";
+        //using dummy keys and tokens
+        private static string CONSUMER_KEY = "CjIO1ihChRmgFaS7FuOHioMbq";
+        private static string CONSUMER_SECRET = "9nrYdCY0jkCUYnPklrjmkdtGvCFb1Y8pP4Dc8JkluOB8RWPRKu";
         private static string ACCESS_TOKEN = "1298079202822914049-8PG8hclia0gqtWJvAWkllc0zJLNui3";
-        private static string ACCESS_TOKEN_SECRET = "oepEPSBF0hokd5RrFRs1SKT3JidkpknZtWlwpj8nbDCOl";
+        private static string ACCESS_TOKEN_SECRET = "oepEPSCF0hokd5RrFRs1SKT3JidkpknZtWlwpj8nbDCOl";
         //private static TwitterClient userClient = new TwitterClient(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
         static async Task Main(string[] args)
         {
